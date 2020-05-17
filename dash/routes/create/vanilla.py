42b2f924c59253fb14b9503496717562b906c9cf
rsakeys <r@rsakeys.org>
2020-05-17 21:31:17 +0100

    Tidy up vanilla module, handle activation in its own module

--- dash/routes/create/vanilla.py
+++ dash/routes/create/vanilla.py
@@ -1,4 +1,4 @@
-from urllib.parse import parse_qs, urlencode
+from urllib.parse import parse_qs
 from email.utils import parseaddr
 from dash.data import db
 from dash.data.penguin import Penguin, ActivationKey
@@ -33,20 +33,6 @@ all_captchas = [
     ('popcorn', Image.open('./dash/templates/images/popcorn.png')),
     ('watermelon', Image.open('./dash/templates/images/watermelon.png'))
 ]
-
-
-@vanilla_activate.get('/<lang>/<code>')
-async def activate(request, lang, code):
-    activation_data = await ActivationKey.query.where(ActivationKey.activation_key == code).gino.first()
-    if not code:
-        return response.json({'message': i18n.t('activate.error', locale=lang)}, status=404)
-    elif not activation_data:
-        return response.json({'message': i18n.t('activate.activation_key_404', locale=lang)}, status=404)
-    else:
-        await Penguin.update.values(active=True) \
-        .where(Penguin.id == activation_data.penguin_id).gino.status()
-        await ActivationKey.delete.where((ActivationKey.penguin_id == activation_data.penguin_id)).gino.status()
-        return response.redirect(app.config.ACTIVATE_REDIRECT)
 
 
 @vanilla_create.get('/<lang>')
@@ -117,18 +103,17 @@ async def create_page(request, lang):
             site_key=app.config.GSITE_KEY
         )
         return response.html(page)
-    else:
-        register_template = env.get_template('create/en.html')
-        page = register_template.render(
-            VANILLA_PLAY_LINK=app.config.VANILLA_PLAY_LINK,
-            anon_token=request['session']['anon_token'],
-            captcha_1=base64_captchas[0],
-            captcha_2=base64_captchas[1],
-            captcha_3=base64_captchas[2],
-            captcha_answer=i18n.t(f'create.{captcha_answer}', locale=lang),
-            site_key=app.config.GSITE_KEY
-        )
-        return response.html(page)
+    register_template = env.get_template('create/en.html')
+    page = register_template.render(
+        VANILLA_PLAY_LINK=app.config.VANILLA_PLAY_LINK,
+        anon_token=request['session']['anon_token'],
+        captcha_1=base64_captchas[0],
+        captcha_2=base64_captchas[1],
+        captcha_3=base64_captchas[2],
+        captcha_answer=i18n.t(f'create.{captcha_answer}', locale=lang),
+        site_key=app.config.GSITE_KEY
+    )
+    return response.html(page)
 
 
 @vanilla_create.post('/<lang>')
@@ -151,8 +136,7 @@ async def register(request, lang):
             },
             status=403
         )
-
-    if trigger == 'name':
+    elif trigger == 'name':
         return await _validate_username(request, post_data, lang)
     elif trigger == 'pass':
         return _validate_password(request, post_data, lang)
@@ -162,79 +146,8 @@ async def register(request, lang):
         return _validate_terms(request, post_data, lang)
     elif trigger == 'captcha':
         return _validate_captcha(request, post_data, lang)
-    else:
-        return await _validate_registration(request, post_data, lang)
-
-
-@vanilla_activate.get('/<lang>')
-async def activate_page(request, lang):
-    if lang == 'fr':
-        register_template = env.get_template('activate/fr.html')
-        page = register_template.render(
-            VANILLA_PLAY_LINK=app.config.VANILLA_PLAY_LINK,
-            site_key=app.config.GSITE_KEY
-        )
-        return response.html(page)
-    elif lang == 'es':
-        register_template = env.get_template('activate/es.html')
-        page = register_template.render(
-            VANILLA_PLAY_LINK=app.config.VANILLA_PLAY_LINK,
-            site_key=app.config.GSITE_KEY
-        )
-        return response.html(page)
-    elif lang == 'pt':
-        register_template = env.get_template('pt_vanilla_activation.html')
-        page = register_template.render(
-            VANILLA_PLAY_LINK=app.config.VANILLA_PLAY_LINK,
-            site_key=app.config.GSITE_KEY
-        )
-        return response.html(page)
-    else:
-        register_template = env.get_template('activate/en.html')
-        page = register_template.render(
-            VANILLA_PLAY_LINK=app.config.VANILLA_PLAY_LINK,
-            site_key=app.config.GSITE_KEY
-        )
-        return response.html(page)
-
-
-@vanilla_activate.post('/<lang>')
-async def activate_page(request, lang):
-    query_string = request.body.decode('UTF-8')
-    post_data = parse_qs(query_string)
-    username = post_data.get('name', [None])[0]
-    activation_code = post_data.get('activationcode', [None])[0]
-    user_data = await Penguin.query.where(Penguin.username == username.lower()).gino.first()
-    activation_data = await ActivationKey.query.where(
-        ActivationKey.activation_key == activation_code
-    ).gino.first()
-    if app.config.GSECRET_KEY:
-        gclient_response = post_data.get('recaptcha_response', [None])[0]
-        async with aiohttp.ClientSession() as session:
-            async with session.post(app.config.GCAPTCHA_URL, data=dict(
-                secret=app.config.GSECRET_KEY,
-                response=gclient_response,
-                remoteip=request.ip
-            )) as resp:
-                gresult = await resp.json()
-                if not gresult['success']:
-                    return response.text(urlencode({
-                        'error': gresult
-                    }))
-    # TODO: Better responses for these?
-    if not username or not activation_code:
-        return response.json({'message': i18n.t('activate.error', locale=lang)}, status=404)
-    elif not activation_data:
-        return response.json({'message': i18n.t('activate.activation_key_404', locale=lang)}, status=404)
-    elif not user_data:
-        return response.json({'message': i18n.t('activate.username_404', locale=lang)}, status=404)
-    else:
-        await Penguin.update.values(active=True).where(
-            Penguin.id == activation_data.penguin_id
-        ).gino.status()
-        await ActivationKey.delete.where((ActivationKey.penguin_id == activation_data.penguin_id)).gino.status()
-        return response.redirect(app.config.ACTIVATE_REDIRECT)
-
+    return await _validate_registration(request, post_data, lang)
+    
 
 async def _validate_registration(request, post_data, lang):
     username = post_data.get('name', [None])[0] 
@@ -269,8 +182,7 @@ async def _validate_registration(request, post_data, lang):
             },
             status=403
         )
-
-    if app.config.GSECRET_KEY:
+    elif app.config.GSECRET_KEY:
         gclient_response = post_data.get('recaptcha_response', [None])[0]
         async with aiohttp.ClientSession() as session:
             async with session.post(app.config.GCAPTCHA_URL, data=dict(
@@ -280,10 +192,7 @@ async def _validate_registration(request, post_data, lang):
             )) as resp:
                 gresult = await resp.json()
                 if not gresult['success']:
-                    return response.text(urlencode({
-                        'error': gresult
-                    }))
-
+                    return response.text('Your captcha score was low, please try again.')
     password = Crypto.hash(password).upper()
     password = Crypto.get_login_hash(password, rndk=app.config.STATIC_KEY)
     password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(12)).decode('utf-8')
@@ -324,10 +233,9 @@ async def _validate_registration(request, post_data, lang):
                 activate_link=f'{app.config.VANILLA_PLAY_LINK}/{lang}/penguin/activate'
             )
         )
-        #sg = SendGridAPIClient(app.config.SENDGRID_API_KEY)
-        #sg.send(message)
+        sg = SendGridAPIClient(app.config.SENDGRID_API_KEY)
+        sg.send(message)
         await ActivationKey.create(penguin_id=penguin.id, activation_key=activation_key)
-
     return response.redirect(app.config.VANILLA_PLAY_LINK)
 
 
@@ -346,7 +254,6 @@ async def _validate_username(request, post_data, lang):
                 'X-Drupal-Ajax-Token': 1
             }
         )
-    
     elif len(username) < 4 or len(username) > 12:
         request['session']['errors']['name'] = True
         return response.json(
@@ -360,7 +267,6 @@ async def _validate_username(request, post_data, lang):
                 'X-Drupal-Ajax-Token': 1
             }
         )
-    
     elif len(re.sub('[^0-9]', '', username)) > 5:
         request['session']['errors']['name'] = True
         return response.json(
@@ -374,7 +280,6 @@ async def _validate_username(request, post_data, lang):
                 'X-Drupal-Ajax-Token': 1
             }
         )
-
     elif re.search('[a-zA-Z]', username) is None:
         request['session']['errors']['name'] = True
         return response.json(
@@ -388,7 +293,6 @@ async def _validate_username(request, post_data, lang):
                 'X-Drupal-Ajax-Token': 1
             }
         )
-
     elif not all(letter.isalnum() or letter.isspace() for letter in username):
         request['session']['errors']['name'] = True
         return response.json(
@@ -402,7 +306,6 @@ async def _validate_username(request, post_data, lang):
                 'X-Drupal-Ajax-Token': 1
             }
         )
-
     nickname = username.rstrip(string.digits)
     names = await db.select([Penguin.username]).where(Penguin.username.like(f'{nickname.lower()}%')).gino.all()
     names = {name for name, in names}
@@ -411,7 +314,6 @@ async def _validate_username(request, post_data, lang):
         max_digits = min(5, 12 - len(nickname))
         usernames_gen = (f'{nickname}{i}' for i in range(1, int('9' * max_digits)) if f'{nickname.lower()}{i}' not in names)
         usernames = [next(usernames_gen) for _ in range(3)]
-        
         if usernames is None:
             return response.json(
                 [
@@ -424,7 +326,6 @@ async def _validate_username(request, post_data, lang):
                     'X-Drupal-Ajax-Token': 1
                 }
             )
-
         return response.json(
                 [
                     _make_name_suggestion(usernames, i18n.t('create.vanilla_name_suggest', locale=lang)),
@@ -436,20 +337,18 @@ async def _validate_username(request, post_data, lang):
                     'X-Drupal-Ajax-Token': 1
                 }
             )
-    
-    else:
-        request['session']['errors']['name'] = False
-        request['session']['username'] = username
-        return response.json(
-            [ 
-                _remove_class('name', 'error'),
-                _add_class('name', 'valid'),
-                _update_errors(request['session']['errors'])
-            ],
-            headers={
-                'X-Drupal-Ajax-Token': 1
-            }
-        )
+    request['session']['errors']['name'] = False
+    request['session']['username'] = username
+    return response.json(
+        [ 
+            _remove_class('name', 'error'),
+            _add_class('name', 'valid'),
+            _update_errors(request['session']['errors'])
+        ],
+        headers={
+            'X-Drupal-Ajax-Token': 1
+        }
+    )
 
 
 def _validate_password(request, post_data, lang):
@@ -467,7 +366,6 @@ def _validate_password(request, post_data, lang):
                 'X-Drupal-Ajax-Token': 1
             }
         )
-    
     elif len(password) < 4:
         request['session']['errors']['pass'] = True
         return response.json(
@@ -481,20 +379,18 @@ def _validate_password(request, post_data, lang):
                 'X-Drupal-Ajax-Token': 1
             }
         )
-    
-    else:
-        request['session']['errors']['pass'] = False
-        request['session']['password'] = password
-        return response.json(
-            [
-                _remove_class('pass', 'error'),
-                _add_class('pass', 'valid'),
-                _update_errors(request['session']['errors'])
-            ],
-            headers={
-                'X-Drupal-Ajax-Token': 1
-            }
-        )
+    request['session']['errors']['pass'] = False
+    request['session']['password'] = password
+    return response.json(
+        [
+            _remove_class('pass', 'error'),
+            _add_class('pass', 'valid'),
+            _update_errors(request['session']['errors'])
+        ],
+        headers={
+            'X-Drupal-Ajax-Token': 1
+        }
+    )
 
 
 async def _validate_email(request, post_data, lang):
@@ -544,20 +440,18 @@ async def _validate_email(request, post_data, lang):
                 'X-Drupal-Ajax-Token': 1
             }
         )
-    
-    else:
-        request['session']['errors']['email'] = False
-        request['session']['email'] = email
-        return response.json(
-            [
-                _remove_class('email', 'error'),
-                _add_class('email', 'valid'),
-                _update_errors(request['session']['errors'])
-            ],
-            headers={
-                'X-Drupal-Ajax-Token': 1
-            }
-        )
+    request['session']['errors']['email'] = False
+    request['session']['email'] = email
+    return response.json(
+        [
+            _remove_class('email', 'error'),
+            _add_class('email', 'valid'),
+            _update_errors(request['session']['errors'])
+        ],
+        headers={
+            'X-Drupal-Ajax-Token': 1
+        }
+    )
 
 
 def _validate_terms(request, post_data, lang):
@@ -575,17 +469,16 @@ def _validate_terms(request, post_data, lang):
                 'X-Drupal-Ajax-Token': 1
             }
         )
-    else:
-        request['session']['errors']['terms'] = False
-        return response.json(
-            [
-                _add_class('terms', 'checked'),
-                _update_errors(request['session']['errors'])
-            ],
-            headers={
-                'X-Drupal-Ajax-Token': 1
-            }
-        )
+    request['session']['errors']['terms'] = False
+    return response.json(
+        [
+            _add_class('terms', 'checked'),
+            _update_errors(request['session']['errors'])
+        ],
+        headers={
+            'X-Drupal-Ajax-Token': 1
+        }
+    )
 
 
 def _validate_captcha(request, post_data, lang):
@@ -597,7 +490,6 @@ def _validate_captcha(request, post_data, lang):
             },
             status=403
         )
-
     elif int(captcha_answer) == int(request['session']['captcha_answer']):
         request['session']['errors']['captcha'] = False
         request['session']['captcha']['passed'] = 1
@@ -610,15 +502,14 @@ def _validate_captcha(request, post_data, lang):
                 'X-Drupal-Ajax-Token': 1
             }
         )
-    else:
-        return response.json(
-            [
-                _make_error_message('captcha', i18n.t('create.captcha_invalid', locale=lang)),
-            ],
-            headers={
-                'X-Drupal-Ajax-Token': 1
-            }
-        )
+    return response.json(
+        [
+            _make_error_message('captcha', i18n.t('create.captcha_invalid', locale=lang)),
+        ],
+        headers={
+            'X-Drupal-Ajax-Token': 1
+        }
+    )
 
 
 def _update_errors(new_setting):
