3de0fd44da933d3995cf111af4131a99b0eb8710
Daan <24899039+Zaseth@users.noreply.github.com>
2020-06-16 18:39:18 +0200

    OCD 1

--- dash/routes/snow/session.py
+++ dash/routes/snow/session.py
@@ -14,8 +14,8 @@ async def snfgenerator(request):
     try:
         query_string = request.body.decode('UTF-8')
         post_data = parse_qs(query_string)
-        res = post_data.get('pid')[0]+ "." + post_data.get('token')[0]
-        return response.json({"hasError": False, "error": "", "data": res})
+        res = post_data.get('pid')[0]+ '.' + post_data.get('token')[0]
+        return response.json({'hasError': False, 'error': '', 'data': res})
     except Exception as error:
         logger.warning(error)
-        return response.json({"hasError": True, "error": "Internal Error", "data": ""}, status=500)
+        return response.json({'hasError': True, 'error': 'Internal Error', 'data': ''}, status=500)
