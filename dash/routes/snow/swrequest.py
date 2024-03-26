0dfe2a7d3cb136d33e745473dd9c2c5254e83b7e
Lekuru <contact@lekuru.xyz>
2024-03-26 16:24:07 +0100

    swrequest: Implement authentication & refactor responses

--- dash/routes/snow/swrequest.py
+++ dash/routes/snow/swrequest.py
@@ -1,18 +1,62 @@
-from sanic import Blueprint, response
+from sanic import Blueprint, HTTPResponse, response
 from sanic.log import logger
 
 from dash import settings
+from dash import app
 
 swrequest = Blueprint('swrequest', url_prefix='/swrequest')
 
+def world_response(
+    token: str,
+    world_name: str,
+    host: str,
+    port: int,
+    owner: str = 'crowdcontrol',
+    branch: str = 'CPNext_dev_branch'
+) -> HTTPResponse:
+    return response.text(
+        f'[S_WORLDLIST]|{token}|{world_name}|{host}|{port}||{owner}|{world_name}|{branch}|example'
+    )
+
+def error_response(
+    code: int,
+    title: str,
+    message: str
+) -> HTTPResponse:
+    return response.text(
+        f'[S_ERROR]|{code}|{title}|{message}'
+    )
 
 @swrequest.get('/')
 async def swhandler(request):
     try:
+        product_name = request.args.get('product_name')
+        world_name = request.args.get('name')
+
+        if not product_name or not world_name:
+            return error_response(3518, 'Cannot Start World', 'Missing parameters')
+
+        if product_name != 'cjsnow' or not world_name.startswith('cjsnow'):
+            return error_response(3514, 'Cannot Start World', 'World type not supported')
+
         token = request.args.get('token')
-        name = request.args.get('name')
-        res = f'[S_WORLDLIST]|{token}|{name}|{settings.CJS_HOST}|{settings.CJS_PORT}||crowdcontrol|cjsnow_0|CPNext_dev_branch|example'
-        return response.text(res)
+        pid = request.args.get('owner')
+
+        if not pid or not token:
+            return error_response(3518, 'Cannot Start World', 'Missing parameters')
+
+        if not pid.isdigit():
+            return error_response(3518, 'Cannot Start World', 'Invalid parameters')
+
+        session_key = await app.ctx.redis.get(f'{pid}.mpsession')
+
+        if not session_key:
+            return error_response(4408, 'Cannot Start World', 'Token timeout')
+
+        if session_key.decode() != token:
+            return error_response(3525, 'Cannot Start World', 'Invalid token')
+
+        return world_response(token, world_name, settings.CJS_HOST, settings.CJS_PORT)
     except Exception as error:
         logger.warning(error)
-        return response.json({'hasError': True, 'error': 'Internal Error', 'data': error}, status=500)
+        return error_response(1, 'Cannot Start World', 'Internal Error')
