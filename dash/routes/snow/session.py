53e1ec48053404fdc6fce6edc11018643c77e34c
Levi <contact@lekuru.xyz>
2024-03-27 12:14:32 +0100

    Implement authentication for metaplace endpoints (#40)
    * snfgenerator: Add authentication
    
    * swrequest: Implement authentication & refactor responses
    
    * snfgenerator: Refactor response message for invalid token

--- dash/routes/snow/session.py
+++ dash/routes/snow/session.py
@@ -1,14 +1,60 @@
-from sanic import Blueprint, response
+from sanic import Blueprint, Request, response
 from sanic.log import logger
+
+from dash.data.penguin import Penguin
+from dash import app
+
+import secrets
 
 session = Blueprint('session', url_prefix='/session')
 
-
 @session.post('/')
-async def snfgenerator(request):
+async def snfgenerator(request: Request):
     try:
-        res = request.form.get('pid') + '.' + request.form.get('token')
-        return response.json({'hasError': False, 'error': '', 'data': res})
+        pid = request.form.get('pid')
+        token = request.form.get('token')
+
+        if not pid or not token:
+            return response.json(
+                {'hasError': True, 'error': 'Missing parameters', 'data': ''},
+                status=400
+            )
+
+        if not pid.isdigit():
+            return response.json(
+                {'hasError': True, 'error': 'Invalid parameters', 'data': ''},
+                status=400
+            )
+
+        username = await Penguin.select('username').where(Penguin.id == int(pid)).gino.first()
+
+        if not username:
+            return response.json(
+                {'hasError': True, 'error': 'User not found', 'data': ''},
+                status=401
+            )
+
+        login_key = await app.ctx.redis.get(f'{username[0]}.loginkey') or b''
+
+        if login_key.decode() != token:
+            return response.json(
+                {'hasError': True, 'error': 'Invalid token', 'data': ''},
+                status=401
+            )
+
+        session_token = secrets.token_hex(16)
+
+        await app.ctx.redis.setex(
+            f'{pid}.mpsession', 60,
+            session_token
+        )
+
+        return response.json(
+            {'hasError': False, 'error': '', 'data': session_token}
+        )
     except TypeError as error:
         logger.warning(error)
-        return response.json({'hasError': True, 'error': 'Internal Error', 'data': ''}, status=500)
+        return response.json(
+            {'hasError': True, 'error': 'Internal Error', 'data': ''},
+            status=500
+        )
